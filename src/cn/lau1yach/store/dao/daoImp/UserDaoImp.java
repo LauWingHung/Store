d4ecc3de5f2b53b6e8b5a59a8548b347c5313a32
Your Name <you@example.com>
2018-08-15 11:18:48 +0800

    lau1yach

---
+++ src/cn/lau1yach/store/dao/daoImp/UserDaoImp.java
@@ -0,0 +1,17 @@
+package cn.lau1yach.store.dao.daoImp;
+
+import cn.lau1yach.store.dao.UserDao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2018/8/15
+ * Time: 11:14
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class UserDaoImp implements UserDao {
+    public static void main(String[] arg) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/cn/lau1yach/store/service/serviceImp/UserServiceImp.java
@@ -0,0 +1,17 @@
+package cn.lau1yach.store.service.serviceImp;
+
+import cn.lau1yach.store.service.UserService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2018/8/15
+ * Time: 11:17
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class UserServiceImp implements UserService {
+    public static void main(String[] arg) {
+        System.out.println("hello world!");
+    }
+}
